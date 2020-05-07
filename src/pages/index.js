d3d6126434ce9b76f3bfe12993a2a540a453c575
Alan Christopher Thomas <alan@smallstep.com>
2020-05-07 07:36:08 -0600

    Use item link for vcard and break into sections

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
 import Link from '@material-ui/core/Link';
 import { AiOutlineContacts } from 'react-icons/ai';
 import { GrMap } from 'react-icons/gr';
@@ -47,11 +48,11 @@ const theme = createMuiTheme({
   },
 });
 
-const ItemLink = ({ icon, href, mb = 1, children }) => (
+const ItemLink = ({ download = undefined, icon, href, mb = 1, children }) => (
   <Typography component="div" variant="body1">
     <Box display="flex" mb={mb}>
       <Box display="flex" alignItems="center" mr={1.5}>{icon}</Box>
-      <Link href={href}>{children}</Link>
+      <Link download={download} href={href}>{children}</Link>
     </Box>
   </Typography>
 );
@@ -104,22 +105,35 @@ const IndexPage = () => (
         <Paper elevation={1}>
           <Box p={4}>
             <Box mb={2}>
-              <Box mb={1}>
-                <Typography variant="h3" component="h1">Alan Christopher Thomas</Typography>
-              </Box>
-              <Box display="flex" >
-                <Typography component="div" variant="subtitle2"><Link download href="/alanchrt.vcf">Contact</Link></Typography>
-                <Box display="flex" alignItems="center" ml={0.75}><Box fontSize={18}><AiOutlineContacts /></Box></Box>
-              </Box>
+              <Typography variant="h3" component="h1">Alan Christopher Thomas</Typography>
             </Box>
 
             <ItemLink icon={<GrMap />} href="https://www.google.com/maps/search/?api=1&query=Windsor,+CO">Windsor, Colorado</ItemLink>
             <ItemLink icon={<FiAtSign />} href="mailto:alan@alanct.com">alan@alanct.com</ItemLink>
             <ItemLink icon={<GrPhone />} href="tel:+18168054272">816-805-4272</ItemLink>
+            <ItemLink download icon={<AiOutlineContacts />} href="/alanchrt.vcf" mb={0}>Contact card (vCard)</ItemLink>
+
+            <Box my={4}>
+              <Divider />
+            </Box>
+
+            <Box mb={2}>
+              <Typography variant="h5">Social</Typography>
+            </Box>
+
             <ItemLink icon={<GrGithub />} href="https://github.com/alanchrt">alanchrt</ItemLink>
             <ItemLink icon={<FiTwitter />} href="https://twitter.com/alanchrt">alanchrt</ItemLink>
             <ItemLink icon={<GrFacebookOption />} href="https://www.facebook.com/alanchrt">alanchrt</ItemLink>
             <ItemLink icon={<GrLinkedinOption />} href="https://www.linkedin.com/in/alanchrt">alanchrt</ItemLink>
+
+            <Box my={4}>
+              <Divider />
+            </Box>
+
+            <Box mb={2}>
+              <Typography variant="h5">Professional</Typography>
+            </Box>
+
             <ItemLink icon={<Tentmaker />} href="https://tentmaker.co" mb={0}>tentmaker</ItemLink>
           </Box>
         </Paper>
