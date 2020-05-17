0f408874fd9257131173b00d8e5b095497f3f309
Alan Christopher Thomas <alan@smallstep.com>
2020-05-16 22:18:26 -0600

    Add openid and microdata

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,11 +50,11 @@ const theme = createMuiTheme({
   },
 });
 
-const ItemLink = ({ download = undefined, icon, href, mb = 1, children }) => (
-  <Typography component="div" variant="body1">
+const ItemLink = ({ download = undefined, icon, href, mb = 1, urlProp, sameAsProp, children, ...props }) => console.log(props) || (
+  <Typography component="div" variant="body1" {...props}>
     <Box display="flex" mb={mb}>
       <Box display="flex" alignItems="center" mr={1.5}>{icon}</Box>
-      <Link download={download} href={href}>{children}</Link>
+      <Link download={download} href={href} {...(urlProp ? { itemProp: 'url' } : {})} {...(sameAsProp ? { itemProp: 'sameAs' } : {})}>{children}</Link>
     </Box>
   </Typography>
 );
@@ -98,21 +98,39 @@ const IndexPage = () => (
           content: 'Personal site for Alan Christopher Thomas (alanchrt)',
         },
       ]}
+      link={[
+        {
+
+          rel: 'openid2.provider',
+          href: 'https://www.google.com/accounts/o8/ud?source=profiles',
+        },
+        {
+          rel: 'openid2.local_id',
+          href: 'https://plus.google.com/102133134317482509497',
+        }
+      ]}
     />
 
     <CssBaseline />
 
-    <Box m={4}>
+    <Box m={4} itemScope itemType="http://schema.org/Person">
       <Container maxWidth="xs">
         <Paper elevation={1}>
           <Box p={4}>
-            <Box mb={2}>
-              <Typography variant="h3" component="h1">Alan Christopher Thomas</Typography>
+            <Box mb={2} itemProp="name">
+              <Typography variant="h3" component="h1">
+                <span itemProp="givenName">Alan</span>{' '}
+                <span itemProp="additionalName">Christopher</span>{' '}
+                <span itemProp="familyName">Thomas</span>
+              </Typography>
             </Box>
 
-            <ItemLink icon={<GrMap />} href="https://www.google.com/maps/search/?api=1&query=Windsor,+CO">Windsor, Colorado</ItemLink>
-            <ItemLink icon={<FiAtSign />} href="mailto:alan@chrt.me">alan@chrt.me</ItemLink>
-            <ItemLink icon={<GrPhone />} href="tel:+18168054272">816-805-4272</ItemLink>
+            <ItemLink icon={<GrMap />} href="https://www.google.com/maps/search/?api=1&query=Windsor,+CO" itemProp="address" itemScope itemType="http://schema.org/PostalAddress">
+              <span itemProp="addressLocality">Windsor</span>,{' '}
+              <span itemProp="addressRegion">Colorado</span>
+            </ItemLink>
+            <ItemLink icon={<FiAtSign />} href="mailto:alan@chrt.me" itemProp="email">alan@chrt.me</ItemLink>
+            <ItemLink icon={<GrPhone />} href="tel:+18168054272" itemProp="telephone">816-805-4272</ItemLink>
             <ItemLink download icon={<AiOutlineContacts />} href="/alanchrt.vcf" mb={0}>Contact card</ItemLink>
 
             <Box my={4}>
@@ -123,11 +141,11 @@ const IndexPage = () => (
               <Typography variant="h5">Social</Typography>
             </Box>
 
-            <ItemLink icon={<GrGithub />} href="https://github.com/alanchrt">alanchrt</ItemLink>
-            <ItemLink icon={<FiTwitter />} href="https://twitter.com/alanchrt">alanchrt</ItemLink>
-            <ItemLink icon={<GrFacebookOption />} href="https://www.facebook.com/alanchrt">alanchrt</ItemLink>
-            <ItemLink icon={<GrLinkedinOption />} href="https://www.linkedin.com/in/alanchrt">alanchrt</ItemLink>
-            <ItemLink icon={<FaAngellist />} href="https://angel.co/u/alanchrt">alanchrt</ItemLink>
+            <ItemLink icon={<GrGithub />} href="https://github.com/alanchrt" sameAsProp>alanchrt</ItemLink>
+            <ItemLink icon={<FiTwitter />} href="https://twitter.com/alanchrt" sameAsProp>alanchrt</ItemLink>
+            <ItemLink icon={<GrFacebookOption />} href="https://www.facebook.com/alanchrt" sameAsProp>alanchrt</ItemLink>
+            <ItemLink icon={<GrLinkedinOption />} href="https://www.linkedin.com/in/alanchrt" sameAsProp>alanchrt</ItemLink>
+            <ItemLink icon={<FaAngellist />} href="https://angel.co/u/alanchrt" sameAsProp>alanchrt</ItemLink>
 
             <Box my={4}>
               <Divider />
@@ -137,8 +155,12 @@ const IndexPage = () => (
               <Typography variant="h5">Professional</Typography>
             </Box>
 
-            <ItemLink icon={<Smallstep />} href="https://smallstep.com">smallstep</ItemLink>
-            <ItemLink icon={<Tentmaker />} href="https://tentmaker.co" mb={0}>tentmaker</ItemLink>
+            <ItemLink icon={<Smallstep />} href="https://smallstep.com" itemProp="worksFor" itemScope itemType="http://schema.org/Organization" urlProp>
+              <span itemProp="name">smallstep</span>
+            </ItemLink>
+            <ItemLink icon={<Tentmaker />} href="https://tentmaker.co" mb={0} itemProp="brand" itemScope itemType="http://schema.org/Organization" urlProp>
+              <span itemProp="name">tentmaker</span>
+            </ItemLink>
           </Box>
         </Paper>
       </Container>
