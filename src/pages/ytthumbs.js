36f029daf1dea8a88827de25ac4e909858fc8395
Alan Christopher Thomas <alan@smallstep.com>
2020-09-30 21:47:08 -0600

    Add maxres demo

---
+++ src/pages/ytthumbs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Page = () => (
+  <div style={{ maxWidth: '940px', margin: '40px auto' }}>
+    <h3>Current</h3>
+    <img src="https://i.ytimg.com/vi/7Gv09kS6_TI/hqdefault.jpg" style={{ width: '100%', marginBottom: '20px' }} />
+    <h3>Max Res</h3>
+    <img src="https://i.ytimg.com/vi/7Gv09kS6_TI/maxresdefault.jpg" style={{ width: '100%', marginBottom: '20px' }} />
+  </div>
+);
+
+export default Page;
